98bc3f71799c54cf4c8c22211a97911a288ce060
Sathurshan sathu <sathurshannn@gmail.com>
2024-02-18 20:55:07 +0530

    step 1 using stepper

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -3,6 +3,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CoursesService} from '../../services/courses.service';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import { courseTitleValidator } from '../../validators/course-title.validator';
+
+interface courseCategory {
+  code:string;
+  description:string;
+}
 
 @Component({
   selector: 'create-course-step-1',
@@ -11,8 +17,44 @@ import {filter} from 'rxjs/operators';
 })
 export class CreateCourseStep1Component implements OnInit {
 
-  ngOnInit() {
+  form = this.fb.group({
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [courseTitleValidator(this.courseService)],
+      updateOn: 'blur'
+    }],
+    releasedAt: [new Date(), [Validators.required]],
+    category: ['BEGINNER', Validators.required],
+    downloadsAllowed: [false, Validators.requiredTrue],
+    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+  });
 
+  courseCategories$: Observable<courseCategory[]>;
+
+  constructor(private fb: FormBuilder, private courseService: CoursesService) {
+
+  }
+
+  ngOnInit() {
+    this.courseCategories$ = this.courseService.findCourseCategories();
+
+    const draft = localStorage.getItem('STEP_1');
+
+    if(draft) {
+      this.form.setValue(JSON.parse(draft));
+    }
+
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid)
+    ).subscribe(val => localStorage.setItem('STEP_1', JSON.stringify(val)));
+  }
+
+  get courseTitle() {
+    return this.form.controls['title'];
   }
 
 }
